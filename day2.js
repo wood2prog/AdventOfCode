38991a20e0188d0c7b1069db9b772b339fb32ee6
Jesse Ogburn <wood2prog@gmail.com>
2024-12-23 14:10:57 -0500

    Add function isLinear

--- day2.js
+++ day2.js
@@ -8,11 +8,33 @@
 
 export const isLinear = function (arrNum) {
   if (
-    arrNum.every((curr, index) => curr < arrNum[index]) ||
-    arrNum.every((curr, index) => curr > arrNum[index])
+    arrNum.every((curr, index) => {
+      if (index === 0) {
+        return true;
+      } else {
+        return curr < arrNum[index - 1];
+      }
+    }) ||
+    arrNum.every((curr, index) => {
+      if (index === 0) {
+        return true;
+      } else {
+        return curr > arrNum[index - 1];
+      }
+    })
   ) {
     return true;
   } else {
     return false;
   }
 };
+
+console.log(
+  [1, 2].every((value, index, arr) => {
+    if (index === 0) {
+      return true;
+    } else {
+      return value > arr[index - 1];
+    }
+  })
+);
