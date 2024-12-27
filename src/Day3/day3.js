2fcb9508e8bdee00af540392a533bce9b6818234
Jesse Ogburn <jogburn88@gmail.com>
2024-12-27 17:53:56 -0500

    Add functions to complete first half of day 3

--- src/Day3/day3.js
+++ src/Day3/day3.js
@@ -1,3 +1,35 @@
+import { read } from "fs";
+import { readFile } from "fs/promises";
+
 export const parseData = function (data) {
-  return [2, 3];
+  const expression = /mul\(\d+,\d+\)/g;
+  const matches = data.match(expression);
+  const results = [];
+  for (let i of matches) {
+    results.push(
+      i
+        .slice(4, i.length - 1)
+        .split(",")
+        .map((value) => Number(value))
+    );
+  }
+  return results;
 };
+
+export const addResults = function (numArr) {
+  let sum = 0;
+  for (let i of numArr) {
+    sum += i[0] * i[1];
+  }
+  return sum;
+};
+
+export const openFile = async function (filePath) {
+  const data = await readFile(filePath, "utf-8");
+  const results = addResults(parseData(data));
+  return results;
+};
+
+openFile("src\\Day3\\memorydump.txt").then((value) =>
+  console.log("The answer is", value)
+);
--- tests/Day3/day3.test.js
+++ tests/Day3/day3.test.js
@@ -1,6 +1,22 @@
 import { test, expect } from "vitest";
 import { parseData } from "../../src/Day3/day3.js";
 
-test("mul(2,3) should return [2,3]", () => {
-  expect(parseData("mul(2,3)")).toStrictEqual([2, 3]);
+test("mul(2,3) should return [[2, 3]]", () => {
+  expect(parseData("mul(2,3)")).toStrictEqual([[2, 3]]);
+});
+
+test("amul(1,2) should return [[1, 2]]", () => {
+  expect(parseData("amul(1,2)")).toStrictEqual([[1, 2]]);
+});
+
+test("amul(1,2)a should return [[1, 2]]"),
+  () => {
+    expect(parseData("amul(1,2)a")).toStrictEqual([[1, 2]]);
+  };
+
+test("amul(1,2)stuffmul(3,4) should return [[1, 2], [3, 4]]", () => {
+  expect(parseData("amul(1,2)stuffmul(3,4)")).toStrictEqual([
+    [1, 2],
+    [3, 4],
+  ]);
 });
