ba16c183b15474899ff5b0cd09bb98e59fe8ce13
Jesse Ogburn <jogburn88@gmail.com>
2024-12-28 20:58:46 -0500

    Add the functions to recreate the basic multiply and add function
    This series of functions is the expanded version of the parseData function. However, there were no seams that I could use to complete the next part of the challenge. So this breaks apart the function enought that I can create conditions for the do and don't functions next

--- src/Day3/day3.js
+++ src/Day3/day3.js
@@ -1,5 +1,8 @@
+import { positionToOffset } from "@vitest/utils";
 import { read } from "fs";
 import { readFile } from "fs/promises";
+import { list } from "postcss";
+import { serialize } from "v8";
 
 export const parseData = function (data) {
   const expression = /mul\(\d+,\d+\)/g;
@@ -24,10 +27,90 @@ export const addResults = function (numArr) {
   return sum;
 };
 
+export const listIndexes = function (str, searchStr) {
+  const locations = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(searchStr, i) >= 0) {
+      locations.push(str.indexOf(searchStr, i));
+      i = locations[locations.length - 1];
+    } else {
+      break;
+    }
+  }
+  return locations;
+};
+
+export const extractNumber = function (str) {
+  let numberResult = "";
+  for (let i of str) {
+    if (Number(i)) {
+      numberResult += i;
+    } else {
+      break;
+    }
+  }
+  if (numberResult) {
+    return Number(numberResult);
+  } else {
+    return NaN;
+  }
+};
+
+export const textTween = function (str, start, end) {
+  const startIndex = str.indexOf(start);
+  const endIndex = str.indexOf(end);
+
+  if (endIndex >= 0 && startIndex >= 0) {
+    return str.slice(startIndex + 1, endIndex);
+  }
+  return undefined;
+};
+
+export const validateNumberPair = function (str) {
+  let numberPair = "";
+
+  if (!str) return undefined;
+
+  if (str.includes(",")) {
+    numberPair = str.split(",").map((value) => Number(value));
+    if (numberPair.every((value) => !isNaN(value))) {
+      return numberPair;
+    }
+  }
+
+  return undefined;
+};
+
+export const listNumberPairs = function (str) {
+  const numberPairs = [];
+  let possiblePair = "";
+  const numberLocations = listIndexes(str, "mul(");
+
+  for (let i of numberLocations) {
+    possiblePair = validateNumberPair(textTween(str.slice(i), "(", ")"));
+    if (possiblePair) {
+      numberPairs.push(possiblePair);
+    }
+  }
+
+  return numberPairs;
+};
+
+// // export const openFile = async function (filePath) {
+// //   const data = await readFile(filePath, "utf-8");
+// //   const results = addResults(parseData(data));
+// //   return results;
+// };
+
 export const openFile = async function (filePath) {
   const data = await readFile(filePath, "utf-8");
-  const results = addResults(parseData(data));
-  return results;
+  const results = listNumberPairs(data);
+  return results
+    .map((value) => value[0] * value[1])
+    .reduce((access, curr) => {
+      return (access += curr);
+    });
 };
 
 openFile("src\\Day3\\memorydump.txt").then((value) =>
--- tests/Day3/day3.test.js
+++ tests/Day3/day3.test.js
@@ -1,5 +1,13 @@
 import { test, expect } from "vitest";
-import { parseData } from "../../src/Day3/day3.js";
+import {
+  parseData,
+  listIndexes,
+  extractNumber,
+  textTween,
+  validateNumberPair,
+  listNumberPairs,
+} from "../../src/Day3/day3.js";
+import exp from "constants";
 
 test("mul(2,3) should return [[2, 3]]", () => {
   expect(parseData("mul(2,3)")).toStrictEqual([[2, 3]]);
@@ -19,4 +27,112 @@ test("amul(1,2)stuffmul(3,4) should return [[1, 2], [3, 4]]", () => {
     [1, 2],
     [3, 4],
   ]);
+});
+
+// listIndexes
+// a function that finds the index of all text string matches and returns the list of indexes
+
+test("a should return 0 when search term is a", () => {
+  expect(listIndexes("a", "a")).toStrictEqual([0]);
+});
+
+test("aba should return 0 2 when search term is a", () => {
+  expect(listIndexes("aba", "a")).toStrictEqual([0, 2]);
+});
+
+test("abc should return 1 if search term is bc", () => {
+  expect(listIndexes("abc", "bc")).toStrictEqual([1]);
+});
+
+//extractNumber
+// starts at index 0 and extracts the number regardless of length
+// assumes the number should always start at the beginning of the string
+// returns the number or NaN
+
+test("1 should return 1", () => {
+  expect(extractNumber("1")).toBe(1);
+});
+
+test("1a should return 1", () => {
+  expect(extractNumber("1a")).toBe(1);
+});
+
+test("12 should return 12", () => {
+  expect(extractNumber("12")).toBe(12);
+});
+
+test("12a should retrun 12", () => {
+  expect(extractNumber("12a")).toBe(12);
+});
+
+test("a1 should return NaN", () => {
+  expect(extractNumber("a1")).toBe(NaN);
+});
+
+// textTween
+// returns the text between first two specified characters
+// if any one of the characters does not exist return undefined
+
+test("abc should return b given a and c", () => {
+  expect(textTween("abc", "a", "c")).toBe("b");
+});
+
+test("ab should return an empty string given a and b", () => {
+  expect(textTween("ab", "a", "b")).toBe("");
+});
+
+test("ab should return undefined given a and c", () => {
+  expect(textTween("ab", "a", "c")).toBe(undefined);
+});
+
+test("ab should return undefined given d and b", () => {
+  expect(textTween("ab", "d", "b")).toBe(undefined);
+});
+
+// validateNumberPair
+// takes a string and checks that the string is composed of two numbers seperated by a comma
+// if true returns a pair array of the two numbers
+// if false returns undefined
+
+test("1,1 should return [1, 1]", () => {
+  expect(validateNumberPair("1,1")).toStrictEqual([1, 1]);
+});
+
+test("a should return undefined", () => {
+  expect(validateNumberPair("a")).toBe(undefined);
+});
+
+test("1,a should return undefined", () => {
+  expect(validateNumberPair("1,a")).toBe(undefined);
+});
+
+test("a,1 should return undefined", () => {
+  expect(validateNumberPair("a,1")).toBe(undefined);
+});
+
+// listNumberPairs
+// should parse a string of data and return all the valid number pairs in an array
+// assume that the string will contain one valid pair at most or no valid pairs
+// return undefined if there are no valid pairs
+test("mul(721,815) should return [[721, 815]]", () => {
+  expect(listNumberPairs("mul(721,815)")).toStrictEqual([[721, 815]]);
+});
+
+test("mul(721,815 should return []", () => {
+  expect(listNumberPairs("amul(721,815")).toStrictEqual([]);
+});
+
+test("mul(721,a) should return []", () => {
+  expect(listNumberPairs("mul(721,a)")).toStrictEqual([]);
+});
+
+test("mul(721,815)mul(721,815) should return [[721, 815], [721, 815]]", () => {
+  expect(listNumberPairs("mul(721,815)mul(721,815)")).toStrictEqual([
+    [721, 815],
+    [721, 815],
+  ]);
+});
+
+test("mul(721,815)mul(721,a) should return [[721, 815]]", () => {
+  expect(listNumberPairs("mul(721,815)mul(721,a)")).toStrictEqual([[721, 815]]);
 });
